822cb249fcc34aec2fe28217b7d85ac9e179cf65
taehee <taehi.dev@gmail.com>
2023-10-18 02:17:11 +0900

    Docs : layout폴더 - 푸터,헤더 추가 와 연결

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
+  <>
+    <Header />
+    <App />
+    <Footer />
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
